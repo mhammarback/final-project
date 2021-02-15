cad9e12f46144cb9f014a6392ba88f0c04aedf92
Moa <63230455+mhammarback@users.noreply.github.com>
2021-02-15 10:55:39 +0100

    Update Summary.js
    manually  updating indentation

--- frontend/src/quiz/Summary.js
+++ frontend/src/quiz/Summary.js
@@ -9,17 +9,17 @@ import { SectionQuiz } from '../lib/Form'
 import { QuizIcon  } from '../lib/Icons'
 
 export const Summary = () => {
-	const summary = useSelector((state) => state.quiz.summary)
-	const dispatch = useDispatch()
+  const summary = useSelector((state) => state.quiz.summary)
+  const dispatch = useDispatch()
 	
   return (
-		<Section>
+    <Section>
       <SectionQuiz>
-		    <h2>We have your results</h2> 
-		      <p>You answered {summary.correctAnswers} correct  out of {summary.numberOfQuestions} questions! </p>
-			      <p>{summary.quote}</p>
-			        <QuizIcon  src={summary.image} alt="" />
-			          <NavLink to="/quiz"><Button onClick={() => dispatch(quiz.actions.restart())}>Restart</Button></NavLink>
+        <h2>We have your results</h2> 
+          <p>You answered {summary.correctAnswers} correct  out of {summary.numberOfQuestions} questions! </p>
+	  <p>{summary.quote}</p>
+            <QuizIcon  src={summary.image} alt="" />
+	      <NavLink to="/quiz"><Button onClick={() => dispatch(quiz.actions.restart())}>Restart</Button></NavLink>
       </SectionQuiz>
     </Section>
   )
